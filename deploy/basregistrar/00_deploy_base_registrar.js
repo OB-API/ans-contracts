9201503ca1e7121a42566b6ae66f7fa9cc02f197
saransh <saranshisat@pm.me>
2021-11-07 13:34:11 +0530

    Added Owned Resolver

--- deploy/basregistrar/00_deploy_base_registrar.js
+++ deploy/basregistrar/00_deploy_base_registrar.js
@@ -21,13 +21,18 @@ module.exports = async ({getNamedAccounts, deployments, network}) => {
 
     const base = await ethers.getContract('BaseRegistrarImplementation');
 
+    const ownedResolver = await ethers.getContract('OwnedResolver')
+
+
     const transactions = []
     transactions.push(await base.addController(owner, {from: deployer}))
     transactions.push(await ens.setSubnodeOwner(ZERO_HASH, sha3('avax'), base.address))
+    transactions.push(await ens.setSubnodeRecord(ZERO_HASH, sha3('avax'), base.address, ownedResolver.address, 0))
+    transactions.push(await base.register(sha3('resolver'), owner, 31536000, {from: deployer}))
     console.log(`Waiting on ${transactions.length} transactions setting base registrar`);
     await Promise.all(transactions.map((tx) => tx.wait()));
 }
 
 
 module.exports.tags = ['baseregistrar'];
-module.exports.dependencies = ['registry']
+module.exports.dependencies = ['registry', 'owned-resolver']
